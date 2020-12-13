ae30105d45e51b4ee01088c6c99d54b70f21078e
Niklas Korz <niklas@niklaskorz.de>
2020-12-13 16:44:22 +0100

    Tweak parameters (finally useful results!)

--- process.sh
+++ process.sh
@@ -3,6 +3,11 @@
 poetry run python -m paralaser.paraphrase \
     --token-lang de \
     --margin absolute \
+    --nprobe 16 \
+    --kmax 8 \
+    --threshold-margin 0.6 \
+    --threshold-faiss 0.6 \
+    --threshold-L2 0.05 \
     -i dev/input.txt \
     -p dev/paraphrases.txt \
 
